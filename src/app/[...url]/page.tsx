27e1afdd09b25eb32f058841aac00d127df0a124
Yash Patel <109963122+PatelYash7@users.noreply.github.com>
2025-01-21 17:49:37 +0530

    rag chat setup with redis.

---
+++ src/app/[...url]/page.tsx
@@ -0,0 +1,32 @@
+import { ragChat } from "@/lib/rag-chat";
+import { redis } from "@/lib/redis";
+
+const reconstructUrl = ({ url }: { url: string[] }) => {
+  const decodedUrls = url.map((url) => decodeURIComponent(url));
+  return decodedUrls.join("/");
+};
+
+export default async function Page({
+  params,
+}: {
+  params: {
+    url: string | string[] | undefined;
+  };
+}) {
+  const { url } = await params;
+  const urls = reconstructUrl({ url: url as string[] });
+  const isAlreadyIndexed = await redis.sismember("indexed-urls", urls);
+  if (!isAlreadyIndexed) {
+    console.log("first")
+    await ragChat.context.add({
+      type: "html",
+      source: urls,
+      config: {
+        chunkOverlap: 50,
+        chunkSize: 200,
+      },
+    });
+    await redis.sadd("indexed-urls", urls);
+  }
+  return <div>Hello Yeahhh</div>;
+}
